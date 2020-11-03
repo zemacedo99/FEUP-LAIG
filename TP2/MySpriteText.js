58c5cc7a2efe4fd9e4e1110630e003734fb47932
Jose Macedo <up201705226@fe.up.pt>
2020-11-03 17:56:03 +0000

    add a reminder

--- TP2/MySpriteText.js
+++ TP2/MySpriteText.js
@@ -1,15 +1,20 @@
 class MySpriteText extends CGFobject  {
-    constructor(scene, text, ss ){
+    constructor(scene, text, spritesheet){
         super(scene);
         this.text = text;
         this.retangle = new MyRectangle(scene,0,0,1,1);
 
-        this.ss = ss;
+        this.spritesheet = spritesheet;
 
     }
 
     display(){
 
+        //TO DO: aplicar a spritesheet com a função .bind()
+
         this.retangle.display();
+
+
+
     }
 }
