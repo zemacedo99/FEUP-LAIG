5ec6761d8ba8778f54ac575eb01b545fd095aebb
zemacedo99 <josedantasmacedo@gmail.com>
2020-12-22 18:34:03 +0000

    mainboard display

--- TP3/MyTile.js
+++ TP3/MyTile.js
@@ -6,6 +6,8 @@ class MyTile{
         this._piece = new MyPiece(scene, "", "")
 
         this.form = new MyHexagon(scene);
+
+        this.texture = new CGFtexture(this.scene, "./scenes/images/tile.png");
     }
 
     get piece() {
@@ -18,9 +20,18 @@ class MyTile{
 
     display()
     {   
+        this.scene.gl.enable(this.scene.gl.BLEND); // enables blending
+        this.scene.gl.blendFunc(this.scene.gl.SRC_ALPHA, this.scene.gl.ONE_MINUS_SRC_ALPHA);// defines the blending function
+
+		
         this.scene.pushMatrix();
-            // this.scene.multMatrix(this.position);
+        this.scene.translate(this.position[0],this.position[1],this.position[2]);
+            this.texture.bind();
             this.form.display();
+            // this._piece.display();
         this.scene.popMatrix();
+
+
+        this.scene.gl.disable(this.scene.gl.BLEND); 
     }
 }
--- TP3/primitives/MyHexagon.js
+++ TP3/primitives/MyHexagon.js
@@ -6,21 +6,32 @@
 class MyHexagon extends CGFobject {
 	constructor(scene) {
 		super(scene);
+		this.width = Math.sqrt(3);
+		this.height = 2; 
 		this.initBuffers();
 	}
 	
 	initBuffers() {
 
 
-        this.vertices = [    
-            0.0,   0.0, 0.0,    // center
-            0.0,   1.0, 0.0,    // top
-            1.0,   0.5, 0.0,    // right top
-            1.0,   -0.5, 0.0,   // right bottom
-            0.0,   -1.0, 0.0,   //  bottom
-            -1.0,   -0.5, 0.0,   // left bottom
-            -1.0,   0.5, 0.0,   // left top
-        ];
+        // this.vertices = [    
+        //     0.0,   0.0, 0.0,    // center
+        //     0.0,   1.0, 0.0,    // top
+        //     1.0,   0.5, 0.0,    // right top
+        //     1.0,   -0.5, 0.0,   // right bottom
+        //     0.0,   -1.0, 0.0,   //  bottom
+        //     -1.0,   -0.5, 0.0,   // left bottom
+        //     -1.0,   0.5, 0.0,   // left top
+		// ];
+		
+		this.vertices = [     
+			0.0,   0.0, 0.0,    //center
+			0.0,   1.0, 0.0,    // top
+		   -1.0,   0.5, 0.0,    // left top
+		   -1.0,  -0.5, 0.0,    // left bottom
+			0.0,  -1.0, 0.0,    // bottom
+			1.0,  -0.5, 0.0,    // right bottom
+			1.0,  0.5,  0.0];   // right top
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
@@ -60,10 +71,13 @@ class MyHexagon extends CGFobject {
         */
 
 		this.texCoords = [
-			0, 1,
-			1, 1,
-			0, 0,
-			1, 0
+			0.5, 0.5,
+			0.5, 0.0,
+			0.0, 0.0,
+			0.0, 1.0,
+			0.5, 1.0,
+			1.0, 1.0,
+			1.0, 0.0,
 		]
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
